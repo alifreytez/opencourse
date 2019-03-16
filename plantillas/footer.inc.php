ddf75a806f98b57ae6c75dbb584b1ee663807657
Hernan <pradoh97@gmail.com>
2019-03-16 08:44:09 -0300

    Plantillas de footer, navbar y head

---
+++ plantillas/footer.inc.php
@@ -0,0 +1,30 @@
+<footer>
+  <div class="contenedor-footer">
+    <div class="margen-general">
+      <div class="cont-flex">
+        <ul>
+          <li>Acerca</li>
+          <li><a href="#">Misión</a></li>
+          <li><a href="#">Visión</a></li>
+          <li><a href="#">Miembros</a></li>
+        </ul>
+        <ul>
+          <li>Tutoría</li>
+          <li><a href="#">Conviértete en profesor</a></li>
+          <li><a href="#">Organiza un grupo de estudio</a></li>
+          <li><a href="#">Inicia una discusión</a></li>
+        </ul>
+        <ul>
+          <li>Ayúdanos</li>
+          <li><a href="#">Sugerencias</a></li>
+          <li><a href="#">Donativos</a></li>
+          <li><a href="#">Formar parte</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="margen-general">
+      <p class="frase">El conocimiento es el hijo del matrimonio de la información y el pensamiento.</p>
+      <p class="derechos">&copy; Freeteach <a href="#">Términos</a> &bull; <a href="#">Privacidad</a></p>
+    </div>
+  </div>
+</footer>
---
+++ plantillas/navbar.inc.php
@@ -0,0 +1,19 @@
+<header>
+  <div class="logotipo">
+    <p class="nombre-sitio">Freeteach</p>
+    <nav>
+      <ul class="enlaces-sitio">
+        <li><a href="#">Cursos</a></li>
+        <li><a href="#">Foros</a></li>
+        <li><a href="#">Misión</a></li>
+        <li><a href="#">Equipo</a></li>
+      </ul>
+    </nav>
+  </div>
+  <div class="menu-sesion">
+    <ul>
+      <li><a href="#">Iniciar sesión</a></li>
+      <li><a href="#">Registrarme</a></li>
+    </ul>
+  </div>
+</header>
