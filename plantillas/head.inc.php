798db79e4501cb8f31c9fd6753b223a275383629
Hernan <pradoh97@gmail.com>
2019-03-20 12:02:17 -0300

    Eliminadas plantillas php

--- plantillas/head.inc.php
+++
@@ -1,7 +0,0 @@
-<meta charset="UTF-8"/>
-<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
-<link rel="stylesheet" href="css/general.css"/>
-<link rel="stylesheet" href="css/header.css"/>
-<link rel="stylesheet" href="css/footer.css"/>
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous"/>
-<link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto|Poiret+One" rel="stylesheet"/>
